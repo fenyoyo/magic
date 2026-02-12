8c5e2a2118e3d937f351babffac9eb6a37a22405
奉有余 <fncong@outlook.com>
2026-02-12 21:42:14 +0800

    first commit

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
